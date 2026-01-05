c8ea4dbae58b777e75cf61508d40818a70add759
Trevor Porter <trev055@hotmail.com>
2026-01-05 22:39:44 +1030

    Add Azure Storage remote state backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,17 @@
 terraform {
   required_version = ">= 1.5"
 
+  backend "azurerm" {
+    resource_group_name  = "rg-tf-state"
+    storage_account_name = "tptfstate01"
+    container_name       = "tfstate"
+    key                  = "cloud-resume.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.100"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
 }
