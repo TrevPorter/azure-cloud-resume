4fb62bca9f3c3fdee8fa68d6327a78be1647c760
Trevor Porter <trev055@hotmail.com>
2026-01-05 22:15:35 +1030

    Use existing resource group instead of managing it

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,13 @@
-resource "azurerm_resource_group" "this" {
-  name     = var.resource_group_name
-  location = var.location
+data "azurerm_resource_group" "this" {
+  name = var.resource_group_name
 }
+
 
 resource "azurerm_storage_account" "this" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.this.name
-  location                 = azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
+  location            = data.azurerm_resource_group.this.location
+
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
@@ -20,7 +21,7 @@ resource "azurerm_storage_account" "this" {
 
 resource "azurerm_cdn_frontdoor_profile" "this" {
   name                = "fd-cloud-resume"
-  resource_group_name = azurerm_resource_group.this.name
+  resource_group_name = data.azurerm_resource_group.this.name
   sku_name            = "Standard_AzureFrontDoor"
 }
 
