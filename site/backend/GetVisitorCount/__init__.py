3bf7a1dfe7c533edd360b620b11456772ec3fff7
Trevor Porter <trev055@hotmail.com>
2026-01-13 22:16:07 +1030

    fixing code for function

--- site/backend/GetVisitorCount/__init__.py
+++ site/backend/GetVisitorCount/__init__.py
@@ -1,36 +1,40 @@
-from azure.identity import DefaultAzureCredential
-from azure.cosmos import CosmosClient
 import os
 import json
+import logging
+
 import azure.functions as func
+from azure.identity import DefaultAzureCredential
+from azure.cosmos import CosmosClient
 
 
-app = func.FunctionApp()
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("GetVisitorCount triggered")
 
-@app.function_name(name="GetVisitorCount")
-@app.route(
-    route="GetVisitorCount",
-    methods=["GET"],
-    auth_level=func.AuthLevel.ANONYMOUS
-)
-def get_visitor_count(req: func.HttpRequest) -> func.HttpResponse:
     endpoint = os.environ["COSMOS_ENDPOINT"]
 
     credential = DefaultAzureCredential()
     client = CosmosClient(endpoint, credential=credential)
 
     database = client.get_database_client("resume")
-    container = database.get_container_client("container")
+    container = database.get_container_client("visits")
 
-    item = container.read_item(
-        item="visitors",
-        partition_key="visitors"
-    )
+    try:
+        item = container.read_item(
+            item="visitors",
+            partition_key="visitors"
+        )
+    except Exception:
+        item = {
+            "id": "visitors",
+            "count": 0
+        }
+        container.create_item(item)
 
     item["count"] += 1
     container.replace_item(item=item, body=item)
 
     return func.HttpResponse(
         json.dumps({"count": item["count"]}),
-        mimetype="application/json"
+        mimetype="application/json",
+        status_code=200
     )
